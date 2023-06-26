7c60e4e5b5874cf5d5fee3a16e5b1ec8502baa3e
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-26 11:07:58 +0530

    [Niketan]Add:Usecase 1 for Flip a Coin to Get Percent to get Heads and Tails

---
+++ Functional_Program/Functional_Program/Flip_Coin_Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Program
+{
+    public class Flip_Coin_Program
+    {
+        public void Flip_Coin() 
+        {
+            Random random = new Random();
+
+            int heads = 0;
+            int tails = 0;
+            int flip = 101;
+            for (int i = 0; i < 10; i++)
+            {
+                int result = random.Next(2);
+                if (result == 0)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+            }
+            double percentHeads = (double)heads / flip * 100;
+            double percentTails = (double)tails / flip * 100;
+            Console.WriteLine("Percentage of Heads are : " + percentHeads + " %");
+            Console.WriteLine("Percentage of Tails are : " + percentTails + " %");
+        }
+    }
+}
--- Functional_Program/Functional_Program/Program.cs
+++ Functional_Program/Functional_Program/Program.cs
@@ -16,6 +16,28 @@ namespace Functional_Program
 
             Console.WriteLine("Welcome to Functional Programs :- ");
 
+            Console.WriteLine("Please select an option for the program to Execute");
+
+            Console.WriteLine("1. Flip Coin And Print Percentage");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    //First Problem
+                    Console.WriteLine("Please Press Enter to Flip a Coin");
+                    Console.ReadLine();
+                    Flip_Coin_Program flip_Coin = new Flip_Coin_Program();
+                    flip_Coin.Flip_Coin();
+                    break;
+
+                default:
+                    Console.WriteLine("Please Enter the Correct Option : ");
+                    break;
+            }
+
+            Console.ReadLine() ;
         }
     }
 }
