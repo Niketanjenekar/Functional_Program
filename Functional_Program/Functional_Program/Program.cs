a9540b4a3a2c193b283b0e39af27ebb5dfa41308
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-26 10:57:32 +0530

    [Niketan]Add:Welcome to Functional Programs

---
+++ Functional_Program/Functional_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****************Functional Programs*****************");
+
+            //*****************Day - 5 Assignments********************
+
+            Console.WriteLine("Welcome to Functional Programs :- ");
+
+        }
+    }
+}
