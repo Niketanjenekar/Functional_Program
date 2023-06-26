47b4c1fe69a574518da1f99ec2786e4133760a00
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-26 11:30:37 +0530

    [Niketan]Add:Usecase 4 for Harmonic Number

---
+++ Functional_Program/Functional_Program/Harmonic_Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Program
+{
+    public class Harmonic_Number
+    {
+        public void Harmonic_Number_Prog(int n)
+        {
+            int result = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                result += 1 + 1 / i;
+            }
+            Console.WriteLine("The " + n + "th Harmonic Value is " + result);
+        }
+    }
+}
